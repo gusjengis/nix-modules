06150e3f0c5dc8c2d4ccc7e8764c279cecad7b01
gusjengis <anthony.j.green@outlook.com>
2026-02-13 22:58:50 -0800

    home assistant seems to be working

--- nixosModules/software/home_assistant.nix
+++ nixosModules/software/home_assistant.nix
@@ -12,27 +12,31 @@
   };
 
   config = lib.mkIf config.homeAssistant.enable {
-    services.home-assistant = {
-      enable = true;
-      extraComponents = [
-        # Components required to complete the onboarding
-        "esphome"
-        "met"
-        "radio_browser"
-      ];
-      config = {
-        # Includes dependencies for a basic setup
-        # https://www.home-assistant.io/integrations/default_config/
-        default_config = { };
-        network = {
-          bind_interfaces = [
-            "wlp4s0"
-            "tailscale0"
-          ];
-        };
+    virtualisation.oci-containers = {
+      backend = "podman";
+      containers.homeassistant = {
+        volumes = [ "home-assistant:/config" ];
+        environment.TZ = "America/Los_Angeles";
+        image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
+        extraOptions = [
+          "--network=host"
+        ];
       };
     };
 
-    networking.firewall.allowedTCPPorts = [ 8123 ];
+    networking.firewall.allowedTCPPorts = [
+      8123 # webU
+      5580 # matter
+    ];
+
+    networking.firewall.allowedUDPPorts = [
+      5353 # mDNS / Zeroconf
+      1900 # SSDP / UPnP
+    ];
+
+    services.matter-server.enable = true;
+    nixpkgs.config.permittedInsecurePackages = [
+      "openssl-1.1.1w" # supposedly matter needs this
+    ];
   };
 }
