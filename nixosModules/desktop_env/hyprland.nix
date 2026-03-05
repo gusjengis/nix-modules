14752195987afd221f58376cdd99bc75d6003f1a
gusjengis <anthony.j.green@outlook.com>
2026-03-05 12:48:01 -0800

    hyprland update

--- nixosModules/desktop_env/hyprland.nix
+++ nixosModules/desktop_env/hyprland.nix
@@ -15,6 +15,9 @@
     programs.hyprland = {
       enable = true;
       xwayland.enable = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      portalPackage =
+        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
 
     services.gnome.gnome-keyring.enable = true;
