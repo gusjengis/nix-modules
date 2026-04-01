58725860eab9100c234be2b8235a9012ebec51e0
gusjengis <anthony.j.green@outlook.com>
2026-04-01 15:15:42 -0700

    misc

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -7,6 +7,10 @@
 }:
 
 {
+  options.repo.networkmanager.enable = lib.mkEnableOption "shared NetworkManager defaults" // {
+    default = true;
+  };
+
   imports = [
     ./hardware/gpu_drivers.nix
     ./hardware/grub.nix
@@ -23,92 +27,94 @@
     ./users.nix
   ];
 
-  grub.enable = lib.mkDefault true;
-  nvidia.enable = lib.mkDefault false;
-  login.gnome.enable = lib.mkDefault false;
-  gnome.enable = lib.mkDefault false;
-  hyprland.enable = lib.mkDefault true;
-  nvim.enable = lib.mkDefault true;
-  git.enable = lib.mkDefault true;
-  vial.enable = lib.mkDefault true;
-  steam.enable = lib.mkDefault false;
-  tailscale.enable = lib.mkDefault true;
-  homeAssistant.enable = lib.mkDefault false;
-  virtual-machines.enable = lib.mkDefault false;
+  config = {
+    grub.enable = lib.mkDefault true;
+    nvidia.enable = lib.mkDefault false;
+    login.gnome.enable = lib.mkDefault false;
+    gnome.enable = lib.mkDefault false;
+    hyprland.enable = lib.mkDefault true;
+    nvim.enable = lib.mkDefault true;
+    git.enable = lib.mkDefault true;
+    vial.enable = lib.mkDefault true;
+    steam.enable = lib.mkDefault false;
+    tailscale.enable = lib.mkDefault true;
+    homeAssistant.enable = lib.mkDefault false;
+    virtual-machines.enable = lib.mkDefault false;
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-  nix.settings.warn-dirty = false;
-
-  services.getty.autologinUser = "gusjengis";
-  services.gvfs.enable = true;
-  services.udisks2.enable = true;
-  services.upower.enable = true;
-  services.power-profiles-daemon.enable = true;
-  time.timeZone = "America/Los_Angeles";
-  networking.networkmanager.enable = true;
-  networking.firewall = {
-    allowedTCPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      }
-      {
-        from = 47984;
-        to = 47990;
-      }
+    nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
     ];
-    allowedUDPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      }
-      {
-        from = 47998;
-        to = 48010;
-      }
-    ];
-    allowedTCPPorts = [
-      48010
-    ];
-  };
-  nixpkgs.config.allowBroken = true;
-  # Turn of password for sudo, so annoying
-  security.sudo.extraConfig = ''
-    %wheel ALL=(ALL) NOPASSWD: ALL
-  '';
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  nixpkgs.config.allowUnfree = true;
-  systemd.services."NetworkManager-wait-online".enable = false;
-  programs.nix-ld.enable = true;
-  programs.dconf.enable = true;
-  services.avahi.publish.enable = true;
-  services.avahi.publish.userServices = true;
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  services.gnome.core-apps.enable = false;
+    nix.settings.warn-dirty = false;
 
-  services.logind.settings.Login = {
-    HandleLidSwitch = "suspend";
-    HandleLidSwitchExternalPower = "ignore";
-    HandleLidSwitchDocked = "ignore";
-  };
+    services.getty.autologinUser = "gusjengis";
+    services.gvfs.enable = true;
+    services.udisks2.enable = true;
+    services.upower.enable = true;
+    services.power-profiles-daemon.enable = true;
+    time.timeZone = "America/Los_Angeles";
+    networking.networkmanager.enable = config.repo.networkmanager.enable;
+    networking.firewall = {
+      allowedTCPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        }
+        {
+          from = 47984;
+          to = 47990;
+        }
+      ];
+      allowedUDPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        }
+        {
+          from = 47998;
+          to = 48010;
+        }
+      ];
+      allowedTCPPorts = [
+        48010
+      ];
+    };
+    nixpkgs.config.allowBroken = true;
+    # Turn of password for sudo, so annoying
+    security.sudo.extraConfig = ''
+      %wheel ALL=(ALL) NOPASSWD: ALL
+    '';
+    hardware.bluetooth.enable = true; # enables support for Bluetooth
+    hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    nixpkgs.config.allowUnfree = true;
+    systemd.services."NetworkManager-wait-online".enable = false;
+    programs.nix-ld.enable = true;
+    programs.dconf.enable = true;
+    services.avahi.publish.enable = true;
+    services.avahi.publish.userServices = true;
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+    services.gnome.core-apps.enable = false;
 
-  fonts.packages = with pkgs; [
-    carlito
-    commit-mono
-    nerd-fonts.meslo-lg
-    helvetica-neue-lt-std
-  ];
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_6_12;
+    services.logind.settings.Login = {
+      HandleLidSwitch = "suspend";
+      HandleLidSwitchExternalPower = "ignore";
+      HandleLidSwitchDocked = "ignore";
+    };
+
+    fonts.packages = with pkgs; [
+      carlito
+      commit-mono
+      nerd-fonts.meslo-lg
+      helvetica-neue-lt-std
+    ];
+    boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_6_12;
+  };
 }
