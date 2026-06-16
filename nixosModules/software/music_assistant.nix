a991db22271610d91e472115522c7f628e4e92f3
gusjengis <anthony.j.green@outlook.com>
2026-06-15 21:51:13 -0700

    free youtube plugin for music assistant

--- nixosModules/software/music_assistant.nix
+++ nixosModules/software/music_assistant.nix
@@ -5,6 +5,14 @@
   ...
 }:
 
+let
+  ytmusicFreeProvider = pkgs.fetchFromGitHub {
+    owner = "sproft";
+    repo = "music-assistant-ytmusic";
+    rev = "20522c3264d7a8bcb9f34c2b25b21b1efd93c087";
+    hash = "sha256-r3opgQGlST2pqca6Nlz2CTZdVGk1wpw6/ia1lZFhW7A=";
+  };
+in
 {
   options = {
     musicAssistant.enable = lib.mkEnableOption "enables Music Assistant";
@@ -15,10 +23,16 @@
 
     systemd.services.musicassistant = {
       description = "Music Assistant";
-      after = [ "docker.service" "network-online.target" ]
-        ++ lib.optionals config.tailscale.enable [ "tailscaled.service" ];
-      wants = [ "docker.service" "network-online.target" ]
-        ++ lib.optionals config.tailscale.enable [ "tailscaled.service" ];
+      after = [
+        "docker.service"
+        "network-online.target"
+      ]
+      ++ lib.optionals config.tailscale.enable [ "tailscaled.service" ];
+      wants = [
+        "docker.service"
+        "network-online.target"
+      ]
+      ++ lib.optionals config.tailscale.enable [ "tailscaled.service" ];
       wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
@@ -26,7 +40,7 @@
         Restart = "always";
         RestartSec = 10;
         ExecStartPre = "-${lib.getExe pkgs.docker} rm -f musicassistant";
-        ExecStart = "${lib.getExe pkgs.docker} run --name=musicassistant --rm --pull=missing --network=host --privileged -v music-assistant:/data -e TZ=America/Los_Angeles ghcr.io/music-assistant/server:latest";
+        ExecStart = "${lib.getExe pkgs.docker} run --name=musicassistant --rm --pull=missing --network=host --privileged -v music-assistant:/data -v ${ytmusicFreeProvider}/ytmusic_free:/app/venv/lib/python3.13/site-packages/music_assistant/providers/ytmusic_free:ro -e TZ=America/Los_Angeles ghcr.io/music-assistant/server:latest";
         ExecStop = "${lib.getExe pkgs.docker} stop musicassistant";
         ExecStopPost = "-${lib.getExe pkgs.docker} rm -f musicassistant";
       };
