c2b7de63b4303459dee8ac48daf392fc0ae50d46
gusjengis <anthony.j.green@outlook.com>
2026-06-16 14:53:30 -0700

    music assistant: bump ytmusic provider to mangle-tolerant URL parsing

--- nixosModules/software/music_assistant.nix
+++ nixosModules/software/music_assistant.nix
@@ -9,8 +9,8 @@ let
   ytmusicFreeProvider = pkgs.fetchFromGitHub {
     owner = "gusjengis";
     repo = "music-assistant-ytmusic";
-    rev = "959937045b226383b4908640622b010ef8bcf20e";
-    hash = "sha256-au0cGe1FD3JoEUvlD1amJyCY/Ed0M7sh/y2JG2cB/Fk=";
+    rev = "c018b06d39b8bee63178e976ea7bdc608c36a60e";
+    hash = "sha256-TTZzeFPMprVIvQlSpaYDhgvuW6hPVy5KES179raI20A=";
   };
 in
 {
