3fcff59dabc2452cfcf990acc7a7adf5f295e5a6
gusjengis <anthony.j.green@outlook.com>
2026-06-17 11:24:59 -0700

    dns change for main mass server

--- nixosModules/software/music_assistant.nix
+++ nixosModules/software/music_assistant.nix
@@ -6,6 +6,8 @@
 }:
 
 let
+  resolvConf = pkgs.writeText "music-assistant-resolv.conf" "nameserver 1.1.1.1\nnameserver 8.8.8.8\noptions edns0\n";
+
   ytmusicFreeProvider = pkgs.fetchFromGitHub {
     owner = "gusjengis";
     repo = "music-assistant-ytmusic";
@@ -40,7 +42,7 @@ in
         Restart = "always";
         RestartSec = 10;
         ExecStartPre = "-${lib.getExe pkgs.docker} rm -f musicassistant";
-        ExecStart = "${lib.getExe pkgs.docker} run --name=musicassistant --rm --pull=missing --network=host --privileged -v music-assistant:/data -v ${ytmusicFreeProvider}/ytmusic_free:/app/venv/lib/python3.14/site-packages/music_assistant/providers/ytmusic_free:ro -e TZ=America/Los_Angeles ghcr.io/music-assistant/server:latest";
+        ExecStart = "${lib.getExe pkgs.docker} run --name=musicassistant --rm --pull=missing --network=host --privileged -v music-assistant:/data -v ${resolvConf}:/etc/resolv.conf:ro -v ${ytmusicFreeProvider}/ytmusic_free:/app/venv/lib/python3.14/site-packages/music_assistant/providers/ytmusic_free:ro -e TZ=America/Los_Angeles ghcr.io/music-assistant/server:latest";
         ExecStop = "${lib.getExe pkgs.docker} stop musicassistant";
         ExecStopPost = "-${lib.getExe pkgs.docker} rm -f musicassistant";
       };
